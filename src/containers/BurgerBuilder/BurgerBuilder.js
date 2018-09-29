c9943db63501bbbcc1e00bdc59f9c2c88e0a6aed
Ryan Owens <rowens794@gmail.com>
2018-09-29 17:27:59 -0400

    added burger creation functionality

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,22 @@
 import React, { Component, Fragment } from 'react';
+import Burger from '../../components/burger/burger';
+
 
 class BurgerBuilder extends Component {
+
+    state = {
+        ingredients: {
+            salad: 0,
+            cheese: 0, 
+            bacon: 0,
+            meat: 0
+        }
+    }
+
     render() {
         return (
             <Fragment>
-                <div>Burger</div>
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build Controls</div>
             </Fragment>
         );
