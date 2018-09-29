b13863a3bf89bb7256ddd9b964fb95ba9b9773d1
Ryan Owens <rowens794@gmail.com>
2018-09-29 11:01:45 -0400

    added css modules to webpack config files

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 const autoprefixer = require('autoprefixer');
 const path = require('path');
@@ -164,6 +164,8 @@ module.exports = {
                 loader: require.resolve('css-loader'),
                 options: {
                   importLoaders: 1,
+                  modules: true,
+                  localIdentName: '[name]__[local]__[hash:base64:5]'
                 },
               },
               {
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 const autoprefixer = require('autoprefixer');
 const path = require('path');
@@ -183,6 +183,8 @@ module.exports = {
                         importLoaders: 1,
                         minimize: true,
                         sourceMap: shouldUseSourceMap,
+                        modules: true,
+                        localIdentName: '[name]__[local]__[hash:base64:5]'
                       },
                     },
                     {
